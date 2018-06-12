36dfcc937ae91fa0e29dc6b835803fca2031590e
bilaleluneis <bilaleluneis@gmail.com>
2018-06-12 11:47:11 -0400

    example of class extension in python

---
+++ classes.py
@@ -0,0 +1,60 @@
+__author__ = "Bilal El Uneis"
+__since__ = "June 2018"
+__email__ = "bilaleluneis@gmail.com"
+
+
+class Vehicle:
+
+    def __init__(self):
+        self.start = False
+        self.stop = True
+        self.turnDirection = None
+        print("Vehicle instance created!")
+
+    def type(self):
+        print("Vehicle")
+
+
+class Car(Vehicle):
+
+    def __init__(self):
+        super().__init__()
+        self.gearNumbers = 3
+        print("Car instance created!")
+
+    def type(self):
+        print("Car")
+
+
+class SportsCar(Car):
+
+    def __init__(self):
+        super().__init__()
+        self.gearNumbers = 6
+        print("Sports Car instance created!")
+
+    def type(self):
+        print("Sports Car")
+
+
+# start of running code
+if __name__ == "__main__":
+
+    carOne = Car()
+    carOne.type()
+    print("number of gears on the car is {}".format(carOne.gearNumbers))
+
+    print("")
+
+    sportsCar = SportsCar()
+    sportsCar.type()
+    print("number of gears on the sports car is {}".format(sportsCar.gearNumbers))
+
+    print("")
+    carOne.type()
+    print("number of gears on the car is {}".format(carOne.gearNumbers))
+
+
+
+
+
