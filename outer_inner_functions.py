2c10b76c3b537bed7f012f7a8580246cf068616e
bilaleluneis <bilaleluneis@gmail.com>
2018-06-25 23:16:44 -0400

    added example of function that takes another function as argument... start to what will become a decorator @

---
+++ outer_inner_functions.py
@@ -0,0 +1,43 @@
+
+__author__ = "Bilal El Uneis"
+__since__ = "June 2018"
+__email__ = "bilaleluneis@gmail.com"
+
+
+def print_message(message):
+
+    def message_printer():
+        print(message)
+
+    return message_printer()
+
+
+def return_message(message):
+
+    def message_printer():
+        print(message)
+
+    return message_printer
+
+
+def simple_function():
+    print("just a simple function")
+
+
+# example of what will become a decorator @
+def decorator_function(another_function):
+    def wrapper_function():
+        return another_function()
+    return wrapper_function
+
+
+# start of running code
+if __name__ == "__main__":
+
+    print_message("printing Hi Bilal!")
+    returned_message = return_message("returning Hi Bilal!")
+    returned_message()
+    decorator = decorator_function(simple_function)
+    decorator()
+
+
