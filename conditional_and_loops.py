f65e08219c5ef9f564a8f66ee021931402d6d095
bilaleluneis <bilaleluneis@gmail.com>
2018-07-19 20:08:12 -0400

    example of using if , while and for statements

---
+++ conditional_and_loops.py
@@ -0,0 +1,30 @@
+
+__author__ = "Bilal El Uneis"
+__since__ = "July 2018"
+__email__ = "bilaleluneis@gmail.com"
+
+
+def if_example(name):
+
+    if name == "Jieshu":
+        for x in name:
+            print(x)
+    else:
+        print("Check failed!")
+
+
+def loop_example(counter):
+
+    while counter > 0:
+        print(counter)
+        counter = counter + 1
+        if counter == 1000:
+            break
+
+
+# start of running code
+if __name__ == "__main__":
+
+    if_example("Bilal")
+    loop_example(20)
+    if_example("Jieshu")
