31ccdf341256d69d33c8726fb133e8500aedbec6
bilaleluneis <bilaleluneis@gmail.com>
2018-07-21 19:43:08 -0400

    minor updates to code to clean up and use better features of the language to solve same problem

--- conditional_and_loops.py
+++ conditional_and_loops.py
@@ -1,12 +1,10 @@
 
-__author__ = "Bilal El Uneis"
-__author__ = "Jieshu"
+__author__ = "Bilal El Uneis and Jieshu Wang"
 __since__ = "July 2018"
 __email__ = "bilaleluneis@gmail.com"
 
 
 def if_example(name):
-
     if name == "Jieshu":
         for x in name:
             if x == "s":
@@ -17,7 +15,6 @@ def if_example(name):
 
 
 def loop_example(counter):
-
     while counter > 0:
         print(counter)
         counter = counter + 1
@@ -27,9 +24,7 @@ def loop_example(counter):
 
 # Jieshu trying
 def find_largest_number_example_jieshu():
-
-    print()
-    print('Find the largest number example:')
+    print('\nFind the largest number example:')
     largest_num_so_far = 0
     print('Initially, the largest number is ', largest_num_so_far)
     for the_num in (9, 41, 12, 3, 89, 16):
@@ -41,9 +36,7 @@ def find_largest_number_example_jieshu():
 
 
 def sum_up_example_jieshu():
-
-    print()
-    print('Sum up exmple:')
+    print('\nSum up example:')
     sum_so_far = 0
     print('Initially, the sum is ', sum_so_far)
     for the_num in (9, 41, 12, 3, 89, 16):
@@ -54,9 +47,7 @@ def sum_up_example_jieshu():
 
 
 def average_example_jieshu():
-
-    print()
-    print('Calculate average example:')
+    print('\nCalculate average example:')
     counter = 0
     sum_so_far = 0
     for the_num in (9, 41, 12, 3, 89, 16):
@@ -69,9 +60,7 @@ def average_example_jieshu():
 
 
 def average_example_jieshu_2(end_num):
-
-    print()
-    print('Average calculation example 2:')
+    print('\nAverage calculation example 2:')
     counter = 0
     sum_so_far = 0
     average_so_far = 0
@@ -83,25 +72,30 @@ def average_example_jieshu_2(end_num):
 
     print('The final average is ', average_so_far, ', approximately', int(average_so_far))
 
-def search_num_example_jieshu(end_num, search_num):
 
-    print()
-    print('Search for the number of "', search_num, '" example:')
+def search_num_example_jieshu(end_num, search_num):
+    print('\nSearch for the number of "', search_num, '" example:')
     found = False
     for the_num in range(0, end_num):
         if the_num == search_num:
             found = True
         print(the_num, found)
-    if found == False:
-        print('The number of "', search_num, '" is not here.')
+    if found:
+        print('We found the number of {} here!'.format(search_num))
     else:
-        print('We found the number of"', search_num, '"here!')
+        print('The number of "', search_num, '" is not here.')
 
+
+def find_in_list(number_to_find):
+    number_found_in_list = None
+    for a_number in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        if a_number == number_to_find:
+            number_found_in_list = a_number
+    print("\nNumber found in list is: ", number_found_in_list)
 
 
 # start of running code
 if __name__ == "__main__":
-
     if_example("Bilal")
     loop_example(20)
     if_example("Jieshu")
@@ -110,3 +104,6 @@ if __name__ == "__main__":
     average_example_jieshu()
     average_example_jieshu_2(18)
     search_num_example_jieshu(15, 23)
+    search_num_example_jieshu(15, 5)
+    find_in_list(10)
+    find_in_list(9)
