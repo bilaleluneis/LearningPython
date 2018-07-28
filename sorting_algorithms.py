0fb7d451874baad952cd94f416f0071e0e1dcaa7
Jieshu <foundwonder@gmail.com>
2018-07-28 01:19:41 -0400

    Write one example of bubble sorting.

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -5,50 +5,44 @@ __email__ = "foundwonder@gmail.com"
 
 from numpy import random as random_generator
 
-# this is the better way to do this.. but will look at it later
-# class TestListsForSorting:
-#
-#     def __init__(self, list_size, end_number):
-#         self.already_sorted_list = list(range(list_size))
-#         self.random_list = random_generator.randint(end_number, size=list_size)
-#         self.reverse_sorted_list = self.already_sorted_list[::-1]
-
-
 # Generate several lists to be sorted
 def lists_generator(end_number, list_size):
     already_sorted_list = list(range(list_size))
-    random_list = random_generator.randint(end_number, size=list_size)
+    random_list = random_generator.randint(end_number, size=list_size).tolist()
     reverse_sorted_list = already_sorted_list[::-1]
     print("The first list to sort is already sorted: {}".format(already_sorted_list))
     print("The second list to sort is random: {}".format(random_list))
     print("The third list to sort is reversely sorted: {}".format(reverse_sorted_list))
     return already_sorted_list, random_list, reverse_sorted_list
 
-
 # Check if a list is sorted correctly
-def sorting_finished_check():
-    counter = 0
-    pass
+def sorting_finished_check(list_to_check):
+    index = 0
+    check = True
+    while index < len(list_to_check) - 1:
+        if list_to_check[index] > list_to_check[index + 1] or check == False:
+            check = False
+        index = index + 1
+    return check
 
 
 def bubble_sort(list_to_be_sorted):
-    list_sorted = list()
-    sorting_finished = False
+    sorting_finished = sorting_finished_check(list_to_be_sorted)
     while not sorting_finished:
-        counter = 0
-        for each_num in list_to_be_sorted:
-            if list_to_be_sorted[counter] > list_to_be_sorted[counter + 1]:
-                list_to_be_sorted.pop(counter)
-                list_to_be_sorted.insert(counter + 1, each_num)
-            counter = counter + 1
-
-
-
+        index = 0
+        while index < len(list_to_be_sorted) - 1:
+            if list_to_be_sorted[index] > list_to_be_sorted[index + 1]:
+                sort_num = list_to_be_sorted[index]
+                list_to_be_sorted.pop(index)
+                list_to_be_sorted.insert(index + 1, sort_num)
+            index = index + 1
+        sorting_finished = sorting_finished_check(list_to_be_sorted)
+    print("Result after sorting is {}".format(list_to_be_sorted))
 
 
 # start of running code
 if __name__ == "__main__":
-    # lists_for_testing = TestListsForSorting(15, 100)
-    (sorted, random, reversed) = lists_generator(100, 15)
+    (sorted, random, reversed) = lists_generator(100, 10)
     bubble_sort(sorted)
     bubble_sort(random)
+    bubble_sort(reversed)
