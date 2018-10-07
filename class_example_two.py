acf0cf0603fbf88aca6fb2848a5f8fee57aeaba9
bilaleluneis <bilaleluneis@gmail.com>
2018-10-06 21:43:56 -0700

    skeleton of classes to use

---
+++ class_example_two.py
@@ -0,0 +1,28 @@
+__author__ = "Bilal El Uneis"
+__since__ = "September 2018"
+__email__ = "bilaleluneis@gmail.com"
+
+
+class Point2D:
+    pass
+
+
+class Point3D(Point2D):
+    pass
+
+
+class Shape3D:
+    pass
+
+
+class Sphere(Shape3D):
+    pass
+
+
+def main():
+    pass
+
+
+# start of running code
+if __name__ == "__main__":
+    main()
