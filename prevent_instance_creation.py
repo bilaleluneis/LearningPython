eaac7a1ab391a03ba78130c6b1c122c7598a26cd
bilaleluneis <bilaleluneis@gmail.com>
2018-11-15 21:25:33 -0700

    update with clear examples of how to prevent direct instantiation.

--- prevent_instance_creation.py
+++ prevent_instance_creation.py
@@ -3,33 +3,43 @@ __since__ = "Nov 2018"
 __email__ = "bilaleluneis@gmail.com"
 
 """
-Examples showing different ways to prevent class instance creation
+Examples showing how to prevent direct class instance creation,
+but allow inheritance and creation of instance of sub class!
 """
 
 
-class InstanceNotAllowed(object):
+# this class can't be instantiated
+class CantInstantiate(object):
 
-    def __init__(self, delay: float = 0.5):
-        self.__delay = delay
+    def __init__(self):
+        print("__init__ called in CantInstantiate !")
 
-    # override the __new__ operator to prevent creating instance of Timer
+    # override the __new__ operator to prevent creating instance of this class
     def __new__(cls, *args, **kwargs):
-        if cls is InstanceNotAllowed:
-            raise TypeError("Not allowed to create instance of Timer !")
+        if cls is CantInstantiate:
+            raise TypeError("CantInstantiate __new__ Not allowed to create instance !")
         return object.__new__(cls)
 
 
-# can create instance of subclass
-class InstanceNotAllowedSubClass(InstanceNotAllowed):
+# CantInstantiate can be inherited and subclass can be instantiated
+class SubClass(CantInstantiate):
 
     def __init__(self):
         super().__init__()
-        print("InstanceNotAllowedSubClass created!")
+        print("__init__ called in SubClass !")
+
+
+# using composition and trying to instantiate CantInstantiate, will expose the behaviour
+class CompositionNotAllowed(object):
+
+    def __init__(self):
+        self.__instance = CantInstantiate()
+        print("__init__ called in CompositionNotAllowed")
 
 
 def main():
-    InstanceNotAllowedSubClass()
-    InstanceNotAllowed()
+    SubClass()
+    CompositionNotAllowed()
 
 
 # start of running code
